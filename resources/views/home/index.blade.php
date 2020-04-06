b2925029eb8a4d3c1426eca8ef1b8c9d588fe0cd
= <andre@bwcenter.com.br>
2020-04-06 17:12:51 -0300

    Mensagen de boas vindas

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,14 @@
 @extends('layout')
 
 @section('title', 'Home')
+
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Bem-vindo ao Free Gym, {{ auth()->user()->name }}!</h6>
+        </div>
+        <div class="card-body">
+            <p>Através desse painel você poderá gerenciar sua academia.</p>
+        </div>
+    </div>
+@endsection
